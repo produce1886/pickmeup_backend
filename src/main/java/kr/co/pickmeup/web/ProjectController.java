b805e52df7749ed037c79f2587a2e17e9e26a30b
Kyeonglin <blossommilktea@gmail.com>
2020-07-26 22:52:05 +0900

    heroku 배포

--- src/main/java/kr/co/pickmeup/web/ProjectController.java
+++ src/main/java/kr/co/pickmeup/web/ProjectController.java
@@ -1,5 +1,7 @@
 package kr.co.pickmeup.web;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import kr.co.pickmeup.domain.projects.Project;
 import kr.co.pickmeup.domain.projects.ProjectRepository;
 import kr.co.pickmeup.service.ProjectService;
@@ -14,7 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@Api(tags = {"1. 프로젝트"})
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value="/projects")
@@ -23,18 +25,21 @@ public class ProjectController {
     private final ProjectService projectService;
 
     // 프로젝트 save
+    @ApiOperation(value="프로젝트 생성", notes="프로젝트를 생성한다.")
     @PostMapping
     public Long save(@RequestBody ProjectSaveRequestDto requestDto) {
         return projectService.save(requestDto);
     }
 
     // 하나 불러오기 (detail)
+    @ApiOperation(value="프로젝트 상세조회", notes="해당 id의 프로젝트를 상세 조회한다.")
     @GetMapping("/{id}")
     public ProjectResponseDto findById(@PathVariable Long id) {
         return projectService.findById(id);
     }
 
     // 전체 불러오기 (pagenation 적용)
+    @ApiOperation(value="전체 프로젝트 조회", notes="모든 프로젝트를 조회한다. 페이지네이션으로 조절가능.")
     @GetMapping
     public ResponseEntity findAll(Pageable pageable) {
         Page<Project> projects = projectService.findAll(pageable);
@@ -42,12 +47,14 @@ public class ProjectController {
     }
 
     // update
+    @ApiOperation(value="프로젝트 수정", notes="해당 id의 프로젝트를 수정한다.")
     @PutMapping("/{id}")
     public Long update(@PathVariable Long id, @RequestBody ProjectUpdateRequestDto requestDto) {
         return projectService.update(id, requestDto);
     }
 
     // delete
+    @ApiOperation(value="프로젝트 삭제", notes="해당 id의 프로젝트를 삭제한다.")
     @DeleteMapping("/{id}")
     public String delete(@PathVariable Long id) {
         projectService.delete(id);
--- src/main/java/kr/co/pickmeup/web/TagController.java
+++ src/main/java/kr/co/pickmeup/web/TagController.java
@@ -1,5 +1,7 @@
 package kr.co.pickmeup.web;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import kr.co.pickmeup.domain.projects.Project;
 import kr.co.pickmeup.domain.tags.Tag;
 import kr.co.pickmeup.service.TagService;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"2. 태그"})
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value="/tags")
@@ -21,6 +24,7 @@ public class TagController {
     private final TagService tagService;
 
     // 전체 불러오기 (pagenation 적용)
+    @ApiOperation(value="전체 태그 조회", notes="모든 태그를 불러온다.")
     @GetMapping
     public ResponseEntity findAll(Pageable pageable) {
         Page<Tag> projects = tagService.findAll(pageable);
